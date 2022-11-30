a8c7af864353174863d7bf19d1e881830c50ad7a
xXHunterSnakeXx <claudiomiranda.89@live.cl>
2022-11-30 03:45:46 -0300

    Json_API_Rick&Morty
    se realizo pagina simple para primera prueba en consumo de api by Hntr

---
+++ Rick_Morty_Api/index.js
@@ -0,0 +1,38 @@
+function getCharacters(done) {
+    const results = fetch("https://rickandmortyapi.com/api/character")
+
+    results    
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+
+        });
+
+}
+
+getCharacters (data => {
+    console.log(data);
+
+    data.results.forEach(personaje => {
+
+        const article = document.createRange().createContextualFragment(/*html*/`
+        
+        <article>
+
+            <div class="image-container">
+                <img src="${personaje.image}" alt="personaje">
+            </div>
+
+        <h2>${personaje.name}</h2>
+        <span>${personaje.status}</span>
+
+        </article>
+        `);   
+
+        const main = document.querySelector("main");
+
+        main.append(article);
+    
+    });
+
+});
